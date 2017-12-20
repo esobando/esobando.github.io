e17a29aacccaa181f2fcd85ede9013b33cdf63b6
esobando <esobando92@gmail.com>
2017-12-20 16:53:18 -0500

    SupermanPage

---
+++ Superman/index3.php
@@ -0,0 +1,32 @@
+<html>
+<?php
+	<body >
+	<table style="width:100%" align="center"> 
+	<tbody style= "background-color: black">
+		<div align="center" valign="center" id="Line">
+			
+			<a href  = "https://esobando.github.io/index.html"><font size="16"> Home </font></a>  &nbsp;  |  &nbsp;
+		
+			
+		
+			<a href = "https://esobando.github.io/Heroes.html" ><font size="23"> DC Heroes </font> </a>&nbsp;  |  &nbsp;
+		
+		
+			<a href = "https://esobando.github.io/Overwatch/MainPage.html" ><font size="16"> Overwatch/AttackSquad </font> </a> &nbsp;  |  &nbsp;
+		
+		
+			<a href = "https://esobando.github.io/Overwatch/TankPage.html" ><font size="16"> Overwatch/TankSquad </font> </a>&nbsp;  |  &nbsp;
+
+			<a href = "https://esobando.github.io/Overwatch/TankPage.html" ><font size="16"> Overwatch/Support </font> </a> 
+			</div>	
+		</tr>
+	</tbody>	
+	</table>
+</body>
+
+
+
+
+?>
+
+</html>
